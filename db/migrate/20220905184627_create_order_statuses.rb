0d5311497318bb13208fd9159b59a1e6da2e29ee
tsekhanskaya <anutta2222@gmail.com>
2022-09-05 21:52:39 +0300

    Added Order Status

---
+++ db/migrate/20220905184627_create_order_statuses.rb
@@ -0,0 +1,10 @@
+class CreateOrderStatuses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :order_statuses do |t|
+      t.integer :number
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_182643) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_184627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_182643) do
     t.text "description"
     t.string "address"
     t.string "img", default: "no_image.jpg"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.integer "number"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
