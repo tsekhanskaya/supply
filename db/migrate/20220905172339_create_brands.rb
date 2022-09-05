a26524a24078594e3b2fb9e0b939e322a55bc1e1
tsekhanskaya <anutta2222@gmail.com>
2022-09-05 20:28:28 +0300

    Added Brand

---
+++ db/migrate/20220905172339_create_brands.rb
@@ -0,0 +1,11 @@
+class CreateBrands < ActiveRecord::Migration[7.0]
+  def change
+    create_table :brands do |t|
+      t.string :title
+      t.text :description
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
