32f6ff28493782cd7292bf35eca3bdac8fc12d82
tsekhanskaya <anutta2222@gmail.com>
2022-09-05 13:07:34 +0300

    init

---
+++ app/views/products/_product.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! product, :id, :brand_id, :title, :price, :description, :img, :created_at, :updated_at
+json.url product_url(product, format: :json)
---
+++ db/migrate/20220905094023_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.integer :brand_id
+      t.string :title, default: 'No title'
+      t.float :price, default: 0
+      t.text :description, default: 'No description'
+      t.string :img, default: 'no_image.jpg'
+
+      t.timestamps
+    end
+  end
+end
