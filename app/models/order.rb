99641b0420cb6e56317ae7f774f2f5ca95b82f15
tsekhanskaya <anutta2222@gmail.com>
2022-09-08 17:57:55 +0300

    Refactoring access to cart (order)

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,16 @@ class Order < ApplicationRecord
   belongs_to :restaurant
   belongs_to :product
   has_one :order_status
+
+  def total_price
+    self.quantity * self.product.price
+  end
+
+  def sub_total
+    sum = 0
+    orders.each do |line_item|
+      sum+= line_item.total_price
+    end
+    sum
+  end
 end
