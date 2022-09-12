24bc9e4e7ee7a7eb746342861d7fd10757f6bc6a
tsekhanskaya <anutta2222@gmail.com>
2022-09-12 16:49:12 +0300

    Refactoring order_items views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'order_items/create'
-  get 'order_items/update'
-  get 'order_items/destroy'
-  get 'cart/show'
+  resources :order_items
+
+  get 'cart', to: 'cart#show'
+
   resources :restaurants
   resources :brands
   devise_for :users
