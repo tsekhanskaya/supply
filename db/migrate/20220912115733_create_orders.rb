828df2c2b35af99ac0c79eaff8d6731f031e9267
tsekhanskaya <anutta2222@gmail.com>
2022-09-12 15:48:04 +0300

    Added model Order

---
+++ db/migrate/20220912115733_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.float :subtotal
+      t.float :total
+      t.integer :restaurant_id
+      t.integer :status_id
+
+      t.timestamps
+    end
+  end
+end
