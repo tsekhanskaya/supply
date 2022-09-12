10d64da4746b97257148467a680443c37775a4db
tsekhanskaya <anutta2222@gmail.com>
2022-09-12 14:04:33 +0300

    Included ApplicationHelper to AppContr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
   before_action :authenticate_user!
   # def after_sign_in_path_for(resource)
   #   root_path
