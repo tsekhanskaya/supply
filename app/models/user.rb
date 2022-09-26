82bcb949fb26f6e0bd81b449a09d8395cc1813d7
tsekhanskaya <anutta2222@gmail.com>
2022-09-26 21:16:41 +0300

    Refactoring default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,6 @@ class User < ApplicationRecord
   after_initialize :set_default_role, if: :new_record?
 
   def set_default_role
-    self.role ||= :user
+    self.role ||= :user_restaurant
   end
 end
