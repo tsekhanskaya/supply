18b3f357839d9876ea9d0dabcddced984ef69fc0
tsekhanskaya <anutta2222@gmail.com>
2022-09-27 12:45:27 +0300

    Refactoring restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,7 +8,7 @@ class RestaurantsController < ApplicationController
     @restaurants = if current_user.admin?
                      Restaurant.all
                    else
-                     Restaurant.where(user_id: current_user)
+                     Restaurant.where(user_id: current_user.id)
                    end
   end
 
